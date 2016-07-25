d3cf96901a081d82c76c4714b927caf597b1b0d4
Maestro <kory.taborn@gmail.com>
2016-07-24 23:18:36 -0500

    Added examples and samples

--- Tests/test_queries.py
+++ Tests/test_queries.py
@@ -1,3 +1,12 @@
-query = YQuery(YHadnler('auth.json'))
+from YHandler import YHandler
+from YHandler import YQuery
+import json
 
-query.
+handler = YHandler('../auth.json')
+query = YQuery(handler, 'nfl')
+leagues = query.get_user_leagues()
+player = query.find_player(leagues[1]['id'], 'Tom Brady')
+stats = query.get_player_season_stats(player[0]['id'])
+f = open('stats.json', 'w')
+json.dump(stats, f)
+f.close()
